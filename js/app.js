01d6ae2a133a81e670a7fa68b83770e94ee069e2
TiborKopca <tibor.kopca@gmail.com>
2024-06-24 15:59:08 +0200

    test

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ xbtn.addEventListener("click", (e) => {
 
 //SCREEN ORIENTATION LOCK
 const DEFAULT_SCREEN_WIDTH = 500;
-window.screen.orientation.addEventListener("change", () => {
+window.screen.orientation.addEventListener("orientationchange", () => {
   document.documentElement.requestFullscreen().then( () => {
     console.log('fullscreen requested');
   })
@@ -63,7 +63,7 @@ window.screen.orientation.addEventListener("change", () => {
   console.log(`screen width: ${screen.width}`);
 
   if(screen.width < DEFAULT_SCREEN_WIDTH) {
-    screen.orientation.lock("portrait");
+    screen.orientation.lock('portrait');
     // screen.orientation.type = "portrait-primary";
     // screen.orientation.lock("portrait-primary");
   } else {
@@ -71,42 +71,102 @@ window.screen.orientation.addEventListener("change", () => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Function to lock the screen orientation to landscape
-  function lockOrientation() {
-      if (screen.orientation) {
-          screen.orientation.lock('landscape')
-              .then(function() {
-                  console.log('Screen orientation locked to landscape');
-              })
-              .catch(function(error) {
-                  console.error('Error locking screen orientation:', error);
-              });
-      } else if (screen.lockOrientation) { // For older browsers
-          screen.lockOrientation('landscape');
-      } else if (screen.mozLockOrientation) { // For Firefox
-          screen.mozLockOrientation('landscape');
-      } else if (screen.msLockOrientation) { // For IE/Edge
-          screen.msLockOrientation('landscape');
-      } else {
-          console.error('Screen Orientation API not supported');
-      }
-  }
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Function to lock the screen orientation to landscape
+//   function lockOrientation() {
+//       if (screen.orientation) {
+//           screen.orientation.lock('landscape')
+//               .then(function() {
+//                   console.log('Screen orientation locked to landscape');
+//               })
+//               .catch(function(error) {
+//                   console.error('Error locking screen orientation:', error);
+//               });
+//       } else if (screen.lockOrientation) { // For older browsers
+//           screen.lockOrientation('landscape');
+//       } else if (screen.mozLockOrientation) { // For Firefox
+//           screen.mozLockOrientation('landscape');
+//       } else if (screen.msLockOrientation) { // For IE/Edge
+//           screen.msLockOrientation('landscape');
+//       } else {
+//           console.error('Screen Orientation API not supported');
+//       }
+//   }
 
-  // Event listener for orientation changes
-  window.addEventListener('orientationchange', function() {
-      if (window.screen.orientation === 90 || window.screen.orientation === -90) {
-          // If the device is in landscape mode
-          lockOrientation();
-      }
-  });
+//   // Event listener for orientation changes
+//   window.addEventListener('orientationchange', function() {
+//       if (window.screen.orientation === 90 || window.screen.orientation === -90) {
+//           // If the device is in landscape mode
+//           lockOrientation();
+//       }
+//   });
 
-  // Initial check in case the device starts in landscape mode
-  if (window.screen.orientation === 90 || window.screen.orientation === -90) {
-      lockOrientation();
-  }
-});
+//   // Initial check in case the device starts in landscape mode
+//   if (window.screen.orientation === 90 || window.screen.orientation === -90) {
+//       lockOrientation();
+//   }
+// });
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Function to lock the screen orientation to landscape
+//   function lockOrientation() {
+//       if (screen.orientation && screen.orientation.lock) {
+//           screen.orientation.lock('portrait')
+//               .then(function() {
+//                   console.log('Screen orientation locked to landscape');
+//               })
+//               .catch(function(error) {
+//                   console.error('Error locking screen orientation:', error);
+//               });
+//       } else {
+//           console.error('Screen Orientation API not supported');
+//       }
+//   }
+
+//   // Request fullscreen and then lock orientation
+//   function requestFullscreenAndLock() {
+//       if (document.documentElement.requestFullscreen) {
+//           document.documentElement.requestFullscreen()
+//               .then(lockOrientation)
+//               .catch(function(error) {
+//                   console.error('Error entering fullscreen:', error);
+//               });
+//       } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+//           document.documentElement.mozRequestFullScreen()
+//               .then(lockOrientation)
+//               .catch(function(error) {
+//                   console.error('Error entering fullscreen:', error);
+//               });
+//       } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+//           document.documentElement.webkitRequestFullscreen()
+//               .then(lockOrientation)
+//               .catch(function(error) {
+//                   console.error('Error entering fullscreen:', error);
+//               });
+//       } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+//           document.documentElement.msRequestFullscreen()
+//               .then(lockOrientation)
+//               .catch(function(error) {
+//                   console.error('Error entering fullscreen:', error);
+//               });
+//       } else {
+//           lockOrientation(); // Fallback if fullscreen is not supported
+//       }
+//   }
+
+//   // Event listener for orientation changes
+//   window.addEventListener('orientationchange', function() {
+//       if (window.orientation === 90 || window.orientation === -90) {
+//           // If the device is in landscape mode
+//           requestFullscreenAndLock();
+//       }
+//   });
+
+//   // Initial check in case the device starts in landscape mode
+//   if (window.orientation === 90 || window.orientation === -90) {
+//       requestFullscreenAndLock();
+//   }
+// });
 
 
 
