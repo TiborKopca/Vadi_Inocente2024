19b01ab21b80b3b69d90c8c6ecc4e6ec588598f1
TiborKopca <tibor.kopca@gmail.com>
2024-06-27 09:52:58 +0200

    changes to layout and sound control. form in the separate page, not pop up from bottom

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-// console.log("vadim")
-
 const subbtn = document.getElementById("subscribe");
-const form = document.getElementById("form");
+// const form = document.getElementById("form");
 const xbtn = document.getElementById("x-button");
 const mainContainer = document.getElementById("container");
 const videoContainer = document.getElementById("videoContainer");
@@ -11,19 +9,17 @@ const videoContainer = document.getElementById("videoContainer");
  FUNCTIONS
 ------------------------------------------------------------
 */
-function getNewsletterIntoView() {
-//   form.scrollIntoView({ behavior: "smooth", block: "end" });
-  form.classList.add("spread");
-  videoContainer.classList.add("opaque");
-  subbtn.classList.remove('btn-active');
+// function getNewsletterIntoView() {
+//   form.classList.add("spread");
+//   videoContainer.classList.add("opaque");
+//   subbtn.classList.remove('btn-active');
   
-}
-function getNewsletterOutOfView() {
-    
-  form.classList.remove("spread");
-  videoContainer.classList.remove("opaque");
-  subbtn.classList.add('btn-active');
-}
+// }
+// function getNewsletterOutOfView() {
+//   form.classList.remove("spread");
+//   videoContainer.classList.remove("opaque");
+//   subbtn.classList.add('btn-active');
+// }
 
 /* 
 ------------------------------------------------------------
@@ -43,16 +39,30 @@ setTimeout(() => {
 EVENT LISTENERS
 ------------------------------------------------------------
 */
-subbtn.addEventListener("click", (e) => {
-    e.preventDefault;
-    if(subbtn.classList.contains("btn-active")) {
-      getNewsletterIntoView();
-    }
-});
+// subbtn.addEventListener("click", (e) => {
+//     e.preventDefault;
+//     if(subbtn.classList.contains("btn-active")) {
+//       getNewsletterIntoView();
+//     }
+// });
 
-xbtn.addEventListener("click", (e) => {
-    e.preventDefault;
-  getNewsletterOutOfView();
+// xbtn.addEventListener("click", (e) => {
+//     e.preventDefault;
+//   getNewsletterOutOfView();
+// });
+
+const video = document.getElementById('videoContainer');
+const muteButton = document.getElementById('playMuted');
+const icon_sound = document.getElementById('playSound');
+
+muteButton.addEventListener('click', function() {
+    if (video.muted) {
+        video.muted = false;
+        icon_sound.src = '../svg/icono_speaker.svg';
+    } else {
+        video.muted = true;
+        icon_sound.src = '../svg/icono_speaker_on.svg';
+    }
 });
 
 
